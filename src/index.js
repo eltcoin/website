94248d271162f9d619e18e9a06d176b86169dcb4
Victor <victor@eltcoin.tech>
2017-10-21 03:32:29 +0100

    Add Store page

--- src/index.js
+++ src/index.js
@@ -7,16 +7,24 @@ import Footer from './components/Footer';
 import Header from './components/Header';
 import ScrollToTop from './components/ScrollToTop';
 import Home from './scenes/Home';
+import Store from './scenes/Store';
 import registerServiceWorker from './registerServiceWorker';
 
 import './i18n';
+
+if (process.env.NODE_ENV === 'production') {
+  window.Raven
+    .config('https://0ddfcefcf922465488c2dde443f9c9d5@sentry.io/230876')
+    .install();
+}
 
 ReactDOM.render(
   <BrowserRouter>
     <ScrollToTop>
       <Background />
       <Header />
-      <Route path="/" component={Home} />
+      <Route exact path="/" component={Home} />
+      <Route path="/store" component={Store} />
       <Footer />
     </ScrollToTop>
   </BrowserRouter>,

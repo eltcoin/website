6151f2f0338831fed8decb0baadcbecd98e938ce
RVerma90 <irahulverma90@gmail.com>
2017-12-01 16:10:55 +0000

    [OKY-255] Team component

--- src/scenes/Home/components/Team/index.js
+++ src/scenes/Home/components/Team/index.js
@@ -1,42 +1,21 @@
 import React, { Component } from 'react';
 
-import Adrian from './images/Adrian.png';
-import Dan from './images/Dan.png';
-import Ivan from './images/Ivan.png';
-import Oliver from './images/Oliver.png';
-import OliverHover from './images/Oliver.hover.png';
-import Oli from './images/Oli.png';
-import Victor from './images/Victor.png';
-import Nathan from './images/Nathan.png';
-import Bot from './images/Bot.png';
+import Adrian from './images/planet1.jpg';
+import Dan from './images/planet2.jpg';
+import Ivan from './images/planet3.jpg';
+import Oliver from './images/planet4.jpg';
+import Oli from './images/planet5.jpg';
+import Victor from './images/planet6.jpg';
+import Nathan from './images/planet7.jpg';
 
 class Team extends Component {
   renderTeamMember(name, role, imageUrl, handle) {
-    handle = handle.replace('@', '');
     return (
       <div className="card">
-        <div className={handle + ' card-image'}>
-          <img className="main" src={imageUrl} alt="" width={200} />
-          <img
-            id="oHover"
-            style={{ opacity: 0, display: 'none' }}
-            src={OliverHover}
-            alt=""
-            width={200}
-          />
-        </div>
+        <img className="main planetStyle" src={imageUrl} alt="" width={100} />
         <div className="card-content">
           <p className="title is-4">{name}</p>
           <p className="subtitle is-6">{role}</p>
-          <p className="handle is-4">
-            <a
-              target="_blank"
-              rel="noopener noreferrer"
-              href={'https://t.me/' + handle}
-            >
-              @{handle}
-            </a>
-          </p>
         </div>
       </div>
     );
@@ -47,71 +26,40 @@ class Team extends Component {
       <section id="team" className="hero">
         <div className="hero-body">
           <div className="container has-text-centered">
-            <h1 className="title is-size-1">TEAM</h1>
+            <h1 className="title is-size-1">Meet the Team</h1>
             <br />
             <div className="columns is-multiline is-mobile">
-              <div className="column is-one-third-tablet is-half-mobile">
-                {this.renderTeamMember(
-                  'Oli H.',
-                  'Founder & Developer',
-                  Oliver,
-                  '@steak_e',
-                )}
+              <div className="column is-one-fifth-desktop is-one-third-tablet is-half-mobile">
+                {this.renderTeamMember('Oli H.', 'Founder & Developer', Oliver)}
               </div>
-              <div className="column is-one-third-tablet is-half-mobile">
+              <div className="column is-one-fifth-desktop is-one-third-tablet is-half-mobile">
                 {this.renderTeamMember(
                   'Victor F.',
                   'Head of Engineering',
                   Victor,
-                  '@vicdu81',
                 )}
               </div>
-              <div className="column is-one-third-tablet is-half-mobile">
+              <div className="column is-one-fifth-desktop is-one-third-tablet is-half-mobile">
                 {this.renderTeamMember(
                   'Ivan S.W.',
                   'Strategist & Senior Advisor',
                   Ivan,
-                  '@augmentedInvestor',
                 )}
               </div>
-              <div className="column is-one-third-tablet is-half-mobile">
-                {this.renderTeamMember(
-                  'Dan T.',
-                  'Head of Design',
-                  Dan,
-                  '@Astroboylambo',
-                )}
+              <div className="column is-one-fifth-desktop is-one-third-tablet is-half-mobile">
+                {this.renderTeamMember('Dan T.', 'Head of Design', Dan)}
               </div>
-              <div className="column is-one-third-tablet is-half-mobile">
-                {this.renderTeamMember(
-                  'Oli M.',
-                  'Senior Advisor & Tech',
-                  Oli,
-                  '@olliemoon',
-                )}
+              <div className="column is-one-fifth-desktop is-one-third-tablet is-half-mobile">
+                {this.renderTeamMember('Oli M.', 'Senior Advisor & Tech', Oli)}
               </div>
-              <div className="column is-one-third-tablet is-half-mobile">
-                {this.renderTeamMember(
-                  'Adrian P.',
-                  'Head of Staff',
-                  Adrian,
-                  '@moonshot100',
-                )}
+              <div className="column is-half-desktop is-one-third-tablet is-half-mobile">
+                {this.renderTeamMember('Adrian P.', 'Head of Staff', Adrian)}
               </div>
-              <div className="column is-half-tablet is-half-mobile">
+              <div className="column is-half-desktop ">
                 {this.renderTeamMember(
                   'Nathan H.',
                   'Head of Community',
                   Nathan,
-                  '@DarkImpulse',
-                )}
-              </div>
-              <div className="column is-half-tablet is-half-mobile">
-                {this.renderTeamMember(
-                  'ELTBOT',
-                  'Head of Automation',
-                  Bot,
-                  '@eltcoin_bot',
                 )}
               </div>
             </div>
